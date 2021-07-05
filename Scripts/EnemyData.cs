c5b7e888c6330d053756fdab712d8804f095387c
Bekhruz Gab <brozermanik@gmail.com>
2021-07-05 23:48:21 +0300

    Assets Folder

---
+++ Scripts/EnemyData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class EnemyData : MonoBehaviour
+{
+    [SerializeField]
+    public float Health;
+    [SerializeField]
+    public float MoveSpeed;
+    [SerializeField]
+    public float AttackSpeed;
+    [SerializeField]
+    public float AttackRange;
+    
+    public enum EnemyType
+    {
+        Human,
+        Orc,
+        Undead,
+        Dragon
+    }
+    
+    
+}
---
+++ Scripts/ProjectileData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileData : MonoBehaviour
+{
+    [SerializeField]
+    public float Damage;
+    [SerializeField]
+    public float ProjectileSpeed;
+    [SerializeField]
+    public float Duration;
+
+    public enum ProjectileType
+    {
+        Bullet,
+        Arrow,
+        MagicMissile
+    }
+}
